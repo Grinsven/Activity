f580cf1b309b7bcbdc459621b3d77d8d35fca1ff
Grinsven <j.vangrinsven@immutec.eu>
2024-11-11 11:08:06 +0100

    first commit

---
+++ Web Applications/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Web_Applications.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
+
